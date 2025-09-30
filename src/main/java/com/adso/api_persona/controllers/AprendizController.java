b1b1253b54a13cc8053f2fb02ce1e9e851387a19
Jhon Deivid Rojas Rodriguez <jhonrojasr253@gmail.com>
2025-09-30 09:50:28 -0500

    Creacion de .env  y configuracion del cros origin

--- src/main/java/com/adso/api_persona/controllers/AprendizController.java
+++ src/main/java/com/adso/api_persona/controllers/AprendizController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/aprendiz")
+@CrossOrigin("http://localhost:5173/")
 public class AprendizController {
     @Autowired
     private AprendizService aprendizService;
---
+++ src/main/java/com/adso/api_persona/repository/AprendizRepository.java
@@ -0,0 +1,7 @@
+package com.adso.api_persona.repository;
+
+import com.adso.api_persona.entities.AprendizEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AprendizRepository extends JpaRepository<AprendizEntity, Long> {
+}
